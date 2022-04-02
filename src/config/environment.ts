015ba348b6eae066393b7726760db233d339b9dd
Yuri Gomes <yuurig@gmail.com>
2022-04-02 15:03:32 -0300

    chore: configure eslint and prettier

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,11 +1,9 @@
-import { cleanEnv, str, port } from 'envalid'
+import { cleanEnv, str, port } from 'envalid';
 
 const environmentDetails = cleanEnv(process.env, {
-    PORT: port({
-        devDefault: 3000
-    })
-})
+  PORT: port({
+    devDefault: 3000
+  })
+});
 
-export {
-    environmentDetails,
-}
+export { environmentDetails };
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,16 +1,17 @@
+import 'dotenv/config';
 import { app } from './app';
 import { environmentDetails } from '../../config/environment';
 
 async function bootstrap() {
   try {
-    app.log.info('Before ready')
+    app.log.info('Before ready');
     await app.ready();
 
-    app.listen(environmentDetails.PORT);
+    await app.listen(environmentDetails.PORT);
   } catch (e) {
     app.log.error(e);
     process.exit(0);
   }
 }
 
-bootstrap()
+bootstrap();
